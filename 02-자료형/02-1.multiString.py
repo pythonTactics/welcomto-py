5d9134a88dfe7d2b8c6b196f37512085d4d0abe2
Koras02 <wjdgh0727@gmail.com>
2023-01-16 13:48:33 +0900

    파이썬 프로그래밍 기초, 자료형 - 02-01.문자열 자료형 - 문자열 연산하기

---
+++ 02-자료형/02-1.multiString.py
@@ -0,0 +1,4 @@
+# 문자열 곱하기 응용
+print("=" * 50)
+print("My Program")
+print("=" * 50)
---
+++ 02-자료형/02.String.py
@@ -0,0 +1,31 @@
+# 문자열
+
+# 큰따옴표 ( " ) 로 양쪽 둘려싸기
+# print("Hello world")
+
+# 작은 따옴표 ( ' ) 로 양쪽 둘러싸기
+# print('Hello World')
+
+# 큰 따옴표 3개(""")로 연속으로 양쪽 둘러싸기
+# print("""Life is Short, and we need Python""")
+
+# 작은 따옴표 3개 연속 (''') 사용
+# print('''Life is Short, and we need Python''')
+
+# 문자열 안에 작은 따옴표나 큰 따옴표를 표현하려 할때
+# food = "Python's favorite food is perl"
+#
+# food
+
+# 문자열 더하기
+# head = "Python"
+# heyon = " is fun!"
+# print(head + heyon)
+
+# 문자열 곱하기
+# a = "python"
+# print(a * 2)
+
+# 문자열 길이 구하기
+a = "Life is too short"
+print(len(a))
